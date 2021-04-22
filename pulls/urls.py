ca35c6875f531fb9af47ff2927f0a2f57310cb43
andimeon <andimeon@gmail.com>
2021-04-22 16:10:19 +0900

    Update urls

--- pulls/urls.py
+++ pulls/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
+from .views import IndexView, RepositoriesView, PullsView
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<str:username>/repositories', views.get_projects, name='repositories'),
-    path('<str:username>/<str:repository_name>/pulls', views.get_project_pulls, name='pulls'),
+    path('', IndexView.as_view(), name='index'),
+    path('<str:username>/repositories', views.RepositoriesView.as_view(), name='repositories'),
+    path('<str:username>/<str:repository_name>/pulls', views.PullsView.as_view(), name='pulls'),
 ]
