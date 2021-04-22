52d525b50600b1f60ca0d4f6cdf2aad231b6e176
andimeon <andimeon@gmail.com>
2021-04-22 16:09:47 +0900

    Update settings

--- github_project/settings.py
+++ github_project/settings.py
@@ -65,7 +65,7 @@ WSGI_APPLICATION = 'github_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'mydatabase',
+        'NAME': 'sqlite.db',
     }
 }
 
