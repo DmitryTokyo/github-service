4a0916678bda8ff390643e9dd2f429a5b38e48ba
andimeon <andimeon@gmail.com>
2021-04-25 23:18:06 +0900

    Create pytest urls

--- pulls/tests/test_urls.py
+++ pulls/tests/test_urls.py
@@ -1,21 +1,18 @@
-from django.test import TestCase
 from django.urls import reverse, resolve
 
-from pulls.views import index, get_projects, get_project_pulls
+from pulls.views import IndexView, RepositoriesView, PullsView
 
 
-class TestUrls(TestCase):
+class TestUrls:
 
-    def test_index_url_is_resolved(self):
-        url = reverse('index')
-        self.assertEquals(resolve(url).func, index)
-        
+    def test_index_url(self):
+        path = reverse('index')
+        assert resolve(path).func.view_class == IndexView
     
-    def test_repositories_url_is_resolved(self):
-        url = reverse('repositories', args={'username': 'buddy'})
-        self.assertEquals(resolve(url).func, get_projects)
-        
+    def test_repositories_url(self):
+        path = reverse('repositories', kwargs={'username': 'dude'})
+        assert resolve(path).func.view_class == RepositoriesView
     
-    def test_pulls_url_is_resolved(self):
-        url = reverse('pulls', args={'username': 'buddy', 'repository_name': 'thebest'})
-        self.assertEquals(resolve(url).func, get_project_pulls)
+    def test_pulls_url(self):
+        path = reverse('pulls', kwargs={'username': 'dude', 'repository_name': 'super'})
+        assert resolve(path).func.view_class == PullsView
